c467e5bfcf8154aac0e30e322c0c3ede1aa42d9a
Shoha-N1 <abduvohidovs85@gmail.com>
2022-12-17 00:14:05 +0500

    Commit

--- js/main.js
+++ js/main.js
@@ -1,42 +1,64 @@
-// var elSearchPokemon = document.querySelector([data - search - pokemon]);
 var elInputHeight = document.querySelector("[data-input-height]");
 var elInputWeight = document.querySelector("[data-input-weight]");
 var elInputImg = document.querySelector("[data-input-img-url]");
 var elInputName = document.querySelector("[data-input-name]");
+var elInputSearch = document.querySelector("[data-input-search]");
 var elForm = document.querySelector("[data-form]");
 var elBox = document.querySelector("[data-Box]");
+var elUl = document.querySelector("[data-ul]");
+var elTemplate = document.querySelector("[data-template]");
+var elSelect = document.querySelector("[data-select]");
+ 
 
-renderPokemon();
-elForm.addEventListener("submit", function (evt)  {
+
+elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   var pokemon = {
     name: null,
     img: null,
     height: null,
     weight: null,
-    type: null,
+    type: [],
   };
 
-      pokemon.name = elInputName.value;
-      pokemon.height = elInputHeight.value;
-      pokemon.weight = elInputWeight.value;
-      pokemon.img = elInputImg.value
+  pokemon.name = elInputName.value;
+  pokemon.height = elInputHeight.value;
+  pokemon.weight = elInputWeight.value;
+  pokemon.img = elInputImg.value;
+  pokemon.search = elInputSearch.value;
 
   pokemons.unshift(pokemon);
   elBox.prepend(createDiv(pokemon));
 });
 
-function renderPokemon() {
+renderPokemons(pokemons)
+
+function renderPokemons(pPokemons) {
   elBox.innerHTML = "";
-  for (var i = 0; i < pokemons.length; i++) {
-    var pokemon = pokemons[i];
+  for (var i = 0; i < pPokemons.length; i++) {
+    var pokemon = pPokemons[i];
+
     elBox.appendChild(createDiv(pokemon));
   }
 }
+
+
 function createDiv(pokemon) {
+  
+  // var elCard = elTemplate.content.cloneNode(true);
+  // elCard.querySelector("img").src = pokemon.img;
+  // elCard.querySelector("[data-card-name]").textContent = pokemon.name;
+  // elCard.querySelector("[data-card-type]").textContent = pokemon.type;
+  // elCard.querySelector("[data-card-weight]").textContent = pokemon.weight;
+  // elCard.querySelector("[data-card-height]").textContent = pokemon.height;
+
+  // elCard.clssList.add("box-pok")
+  // return elCard
+
+
   var elDiv = document.createElement("div");
   var elImg = document.createElement("img");
-  var elSpan = document.createElement("span")
+  var elSpan = document.createElement("span");
   var elH2 = document.createElement("h2");
   var elP = document.createElement("p");
   var elWeight = document.createElement("h3");
@@ -46,17 +68,51 @@ function createDiv(pokemon) {
   elH2.textContent = `${pokemon.name}`;
   elWeight.textContent = `${pokemon.weight}`;
   elHeight.textContent = `${pokemon.height}`;
-
+  elP.textContent = `${pokemon.type}`;
 
   elDiv.appendChild(elImg);
-  elDiv.appendChild(elSpan)
+  elDiv.appendChild(elSpan);
   elDiv.appendChild(elH2);
   elDiv.appendChild(elP);
   elDiv.appendChild(elWeight);
   elDiv.appendChild(elHeight);
 
   elDiv.classList.add("box-pok");
-  
   return elDiv;
 }
 
+
+elInputSearch.addEventListener("keyup", (evt) => {
+  var newPokemons = [];
+  pokemons.forEach((pokemon) => {
+    if (pokemon.name.includes(elInputSearch.value)) {
+      newPokemons.push(pokemon);
+    }
+  });
+  renderPokemons(newPokemons);
+});
+
+
+// function pokemonName() {
+//   elBox.innerHTML = "";
+//   pokemons.forEach((pokemon) => {
+//     var elDiv = elTemplate.content.cloneNode(true);
+//     elDiv.querySelector("img").textContent = pokemon.img;
+//     elDiv.querySelector("[data-card-name]") = pokemon.name;
+//     elDiv.querySelector("[data-card-type]") = pokemon.type;
+//     elDiv.querySelector("[data-card-weight]") = pokemon.weight;
+//     elDiv.querySelector("[data-card-height]") = pokemon.height;
+    
+//     elBox.appendChild(elDiv);
+//   });
+// }
+
+
+elSelect.addEventListener("click", (evt) => {
+  var elType = pokemons.filter((pokemon) =>
+    pokemon.type.includes(elSelect.value)
+
+  );
+
+  renderPokemons(elType);
+});
--- js/pokemon.js
+++ js/pokemon.js
@@ -1,3 +1,5 @@
+
+
 var pokemons = [
     {
       id: 1,
