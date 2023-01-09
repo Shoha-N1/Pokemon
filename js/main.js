077b89e795acf4266a9ce69933489417200ac5c0
Shoha-N1 <abduvohidovs85@gmail.com>
2023-01-09 14:53:56 +0500

    Commit

--- js/main.js
+++ js/main.js
@@ -9,43 +9,9 @@ let elUl = document.querySelector("[data-ul]");
 let elTemplate = document.querySelector("[data-template]");
 let elSelect = document.querySelector("[data-select]");
 let elSelectSort = document.querySelector("[data-select-sort]");
-
+let elFavorites = document.querySelector("[data-favorites]")
 
 let favorites = getFavorites();
-
-elBox.addEventListener("click", (evt) => {
-
-  onFavoriteClick(evt)
-})
-
-
-function onFavoriteClick(evt){
-  let el = evt.target.closest("[data-card-delete]")
-
-  if(!el) return;
-
-  let id = el.dataset.id;
-
-  
- 
-  if (favorites.includes(id)){
-    favorites.splice(favorites.indexOf(id), 1)
-  }
-  else{
-    favorites.push(id)
-  }
-  setFavorites(favorites)
-  renderPokemons(pokemons)
-}
-
-function setFavorites(favorites){
-  localStorage.setItem("favorites", JSON.stringify(favorites));
-}
-
-function getFavorites(){
-  let stringFav = localStorage.getItem("favorites") || "[]"
-  return JSON.parse(stringFav)
-}
 
 
 elForm.addEventListener("submit", function (evt) {
@@ -86,52 +52,68 @@ function renderPokemons(pPokemons) {
 
 function createDiv(pokemon) {
 
-  let elDiv = document.createElement("div");
-  let elImg = document.createElement("img");
-  let elSpan = document.createElement("span");
-  let elH2 = document.createElement("h2");
-  let elHeart = document.createElement("b")
-  let elP = document.createElement("p");
-  let elWeight = document.createElement("h3");
-  let elHeight = document.createElement("h3");
-  let elButton = document.createElement("button");
+  let elCard = elTemplate.content.cloneNode(true);
+ 
+  elCard.querySelector("[data-img-temp]").src = pokemon.img;
+  elCard.querySelector("[data-card-name]").textContent = pokemon.name;
+  elCard.querySelector("[data-card-type]").textContent = pokemon.type;
+  elCard.querySelector("[data-card-weight]").textContent = pokemon.weight;
+  elCard.querySelector("[data-card-height]").textContent = pokemon.height;
+  elCard.querySelector("[data-card-add]").dataset.id = pokemon.id;
+  elCard.querySelector("[data-card-add]").textContent = favorites.includes(pokemon.id) ? "Added" : "Add";
+
+
+
+ 
+  return elCard
+  
+
+  // let elDiv = document.createElement("div");
+  // let elImg = document.createElement("img");
+  // let elSpan = document.createElement("span");
+  // let elH2 = document.createElement("h2");
+  // let elHeart = document.createElement("b")
+  // let elP = document.createElement("p");
+  // let elWeight = document.createElement("h3");
+  // let elHeight = document.createElement("h3");
+  // let elButton = document.createElement("button");
 
   
-  elButton.dataset.id = pokemon.id;
-  elButton.textContent = favorites.includes(pokemon.id) ? "Added" : "Add";  
+  // elButton.dataset.id = pokemon.id;
+  // elButton.textContent = favorites.includes(pokemon.id) ? "Added" : "Add";  
   
 
-  elButton.classList.add("button-1");
+  // elButton.classList.add("button-1");
 
-  elButton.textContent = "Add";
-  elImg.src = `${pokemon.img}`;
-  elH2.textContent = `${pokemon.name}`;
+  // elButton.textContent = "Add";
+  // elImg.src = `${pokemon.img}`;
+  // elH2.textContent = `${pokemon.name}`;
 
-  elHeart.textContent = "❤";
-  elHeart.classList.add("heart")
-  elHeart.addEventListener("click", () => {
-    // elHeart.classList.add("hert")
-    // elHeart.style.color = "red"  elHeart.style.color = "white"
+  // elHeart.textContent = "❤";
+  // elHeart.classList.add("heart")
+  // elHeart.addEventListener("click", () => {
+  //   // elHeart.classList.add("hert")
+  //   // elHeart.style.color = "red"  elHeart.style.color = "white"
     
-  })
+  // })
 
   
 
-  elWeight.textContent = `${pokemon.weight}`;
-  elHeight.textContent = `${pokemon.height}`;
-  elP.textContent = `${pokemon.type}`; 
+  // elWeight.textContent = `${pokemon.weight}`;
+  // elHeight.textContent = `${pokemon.height}`;
+  // elP.textContent = `${pokemon.type}`; 
 
-  elDiv.appendChild(elButton);
-  elDiv.appendChild(elImg);
-  elDiv.appendChild(elSpan);
-  elDiv.appendChild(elH2);
-  elDiv.appendChild(elHeart)
-  elDiv.appendChild(elP);
-  elDiv.appendChild(elWeight);
-  elDiv.appendChild(elHeight);
+  // elDiv.appendChild(elButton);
+  // elDiv.appendChild(elImg);
+  // elDiv.appendChild(elSpan);
+  // elDiv.appendChild(elH2);
+  // elDiv.appendChild(elHeart)
+  // elDiv.appendChild(elP);
+  // elDiv.appendChild(elWeight);
+  // elDiv.appendChild(elHeight);
 
-  elDiv.classList.add("box-pok");
-  return elDiv;
+  // elDiv.classList.add("box-pok");
+  // return elDiv;
   
   
 }
@@ -147,19 +129,52 @@ elInputSearch.addEventListener("keyup", (evt) => {
   renderPokemons(newPokemons);
 });
 
-// function pokemonName() {
-//   elBox.innerHTML = "";
-//   pokemons.forEach((pokemon) => {
-//     let elDiv = elTemplate.content.cloneNode(true);
-//     elDiv.querySelector("img").textContent = pokemon.img;
-//     elDiv.querySelector("[data-card-name]") = pokemon.name;
-//     elDiv.querySelector("[data-card-type]") = pokemon.type;
-//     elDiv.querySelector("[data-card-weight]") = pokemon.weight;
-//     elDiv.querySelector("[data-card-height]") = pokemon.height;
 
-//     elBox.appendChild(elDiv);
-//   });
-// }
+
+elBox.addEventListener("click", (evt) => {
+
+  onFavoriteClick(evt)
+})
+
+
+function onFavoriteClick(evt){
+  let el = evt.target.closest("[data-card-add]")
+
+  if(!el) return;
+
+  let id = el.dataset.id;
+
+  
+ 
+  if (favorites.includes(id)){
+    favorites.splice(favorites.indexOf(id), 1)
+  }
+  else{
+    favorites.push(id)
+  }
+  setFavorites(favorites)
+  renderPokemons(pokemons)
+}
+
+function setFavorites(favorites){
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  renderFavorites(favorites)
+}
+
+function getFavorites(){
+  let stringFav = localStorage.getItem("favorites") || "[]"
+  return JSON.parse(stringFav)
+}
+
+function renderFavorites(favorites){
+  let html = "";
+  favorites.forEach(item => {
+    let pokemon = pokemons.find((pokemon) => (pokemon.id === item));
+    html += `${pokemon.name}` ;
+  });
+  elFavorites.innerHTML = html;
+}
+
 
 elSelect.addEventListener("click", (evt) => {
   let elType = pokemons.filter((pokemon) =>
